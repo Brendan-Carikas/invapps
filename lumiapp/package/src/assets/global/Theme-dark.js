25d200c937cebca14c367feb08f02f53bf93783c
Brendan Carikas <brendan.carikas@gmail.com>
2025-01-03 18:53:43 +0000

    Lumi update
    Theme tidy ups

--- lumiapp/package/src/assets/global/Theme-dark.js
+++ lumiapp/package/src/assets/global/Theme-dark.js
@@ -103,6 +103,7 @@ const darkTheme = createTheme({
       styleOverrides: {
         root: {
           borderRadius: "8px",
+          padding: "14px",
           backgroundColor: '#0B1215 !important',
           backgroundImage: 'none !important',
           boxShadow: "0 1px 3px 0 rgb(0 0 0 / 0.3), 0 1px 2px -1px rgb(0 0 0 / 0.3)",

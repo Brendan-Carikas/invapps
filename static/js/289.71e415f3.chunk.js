fb4bb5d6b60fee5f3a33e59d3a6a00bdae280f83
Brendan Carikas <brendan.carikas@gmail.com>
2025-01-08 06:28:59 +0000

    Updates

---
+++ static/js/289.71e415f3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkflexy=self.webpackChunkflexy||[]).push([[289],{3289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.71e415f3.chunk.js.map
